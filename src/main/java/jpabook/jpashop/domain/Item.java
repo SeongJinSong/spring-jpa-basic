0fea49aca957229dfca4c23bd376d63ca99c9972
seongjin_song <seongjin_song@tmax.co.kr>
2022-01-16 02:41:30 +0900

    add : 실전 예제 4 - 상속관계 매핑

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
+public abstract class Item extends BaseEntity{
     @Id @GeneratedValue
     @Column(name = "item_id")
     private Long id;
-
     private String name;
     private int price;
     private int stockQuantity;
---
+++ src/main/java/jpabook/jpashop/domain/Movie.java
@@ -0,0 +1,6 @@
+package jpabook.jpashop.domain;
+
+public class Movie extends Item{
+    private String director;
+    private String actor;
+}
